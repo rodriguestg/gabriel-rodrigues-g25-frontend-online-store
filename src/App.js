0a5d950a19e7a6309c5240484aea8714ac27f6f4
Jonas Macedo <jonas.macedo19@gmail.com>
2022-07-07 16:41:56 -0300

    Iniciando requisito 1.

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        > 
+          {/* Learn React  */}
         </a>
       </header>
     </div>
