c86ae2a3c4328239dbe194c13ad82cff6904fa1a
Jonas Macedo <jonas.macedo19@gmail.com>
2022-07-07 17:29:00 -0300

    Execução do requisito 1.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 export async function getCategories() {
-  // Implemente aqui
+  const resposta = await fetch(`https://api.mercadolibre.com/sites/MLB/categories`);
+  const categories = resposta.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const resposta = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const categoriesAndQuery = resposta.json();
+  return categoriesAndQuery;
 }
+
