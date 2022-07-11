4b8322f56354ad9fb32164f08c3cd9a276aed107
Carlos Arthur <arthurandrade918@gmail.com>
2022-07-11 16:24:27 -0300

    terminando requisito 7

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
+import ProductDetail from './pages/ProductDetail';
 
 class App extends Component {
   // Renderiza as rotas da aplicação.
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={ Home } />
             <Route exact path="/shoppingcart" component={ ShoppingCart } />
+            <Route exact path="/productdetail/:id" component={ ProductDetail } />
           </Switch>
         </BrowserRouter>
       </>
---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ProductDetail extends Component {
+  state = {
+    idInfos: {},
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const ENDPOINT = `https://api.mercadolibre.com/items/${id}`;
+    const response = await fetch(ENDPOINT);
+    const idInfos = await response.json();
+    this.setState({ idInfos });
+  }
+
+  render() {
+    const { idInfos } = this.state;
+    return (
+      <div>
+        <p data-testid="product-detail-name">{ idInfos.title }</p>
+        <img alt="" src={ idInfos.thumbnail } />
+        <p>{ idInfos.price }</p>
+      </div>
+    );
+  }
+}
+
+export default ProductDetail;
+
+ProductDetail.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.objectOf(PropTypes.string),
+  }).isRequired,
+};
